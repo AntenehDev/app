edf4ea9f20c053b36f1fec52e02d95dc86d85444
Anteneh <45298436+AntenehDev@users.noreply.github.com>
2022-11-20 03:30:48 -0800

    add multiples of function

--- index.py
+++ index.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'hello, world'
+
+@app.route('/<int:num>')
+def mul(num):
+    multiples=[]
+    for i in range(1, 11):
+        sum = num * i
+        multiples.append(sum)
+    return multiples
     
 if __name__ == '__main__':
     app.run(debug=False)
